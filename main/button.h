46cf5cd7b91d869dfadfe64f68a10beea6502751
Antoine Hickey <antoinehickey123@gmail.com>
2025-08-04 12:56:51 -0400

    formatting

--- main/button.h
+++ main/button.h
@@ -1,28 +1,29 @@
 #pragma once
 #include <Arduino.h>
 
-#define BUTTON_PIN 8 // Pin for the button
-#define BUTTON_ENABLED true
+#define BUTTON_PIN          8                   // Pin for the button
+#define BUTTON_ENABLED      true                // Enable or disable button functionality
 
-#define DEFAULT_MODE BRAKE_MODE_DYNAMIC // pick from the enum below
+#define DEFAULT_MODE        BRAKE_MODE_DYNAMIC  // pick from the enum below
 
 enum BrakeMode {
-    BRAKE_MODE_DYNAMIC, // Brake lights are on based on how hard you brake
-    BRAKE_MODE_STATIC, // Brake lights are all on when braking
+    BRAKE_MODE_DYNAMIC,                         // Brake lights are on based on how hard you brake
+    BRAKE_MODE_STATIC,                          // Brake lights are all on when braking
     
-    BRAKE_MODE_HAZARDS,
-    BRAKE_MODE_CHROMA, // Chroma mode
-    BRAKE_MODE_CHRISTMAS, // Christmas mode
-    BRAKE_MODE_HALLOWEEN, // Halloween mode
-    BRAKE_MODE_FLASHLIGHT, // Flashlight mode
+    BRAKE_MODE_HAZARDS,                         // Hazard lights mode
+    BRAKE_MODE_CHROMA,                          // Chroma mode
+    BRAKE_MODE_CHRISTMAS,                       // Christmas mode
+    BRAKE_MODE_HALLOWEEN,                       // Halloween mode
+    BRAKE_MODE_FLASHLIGHT,                      // Flashlight mode
     
-    BRAKE_MODE_NUM_MODES // Placeholder DO NOT REMOVE, SHOULD ALWAYS BE LAST
+    BRAKE_MODE_NUM_MODES                        // Placeholder DO NOT REMOVE, SHOULD ALWAYS BE LAST
 };
 
 class Button {
-public:
-    bool state = false;
-    bool prevState = false;
-    int mode = DEFAULT_MODE;
-    void update();
+    public:
+        bool        state =        false;
+        bool        prevState =    false;
+        int         mode =         DEFAULT_MODE;
+        
+        void update();
 };
--- main/gyro.h
+++ main/gyro.h
@@ -3,66 +3,66 @@
 #include "button.h"
 
 // green light on gyro towards back
-#define MPU6050_ADDR 0x68  // MPU6050 I2C address
+#define MPU6050_ADDR                0x68           // MPU6050 I2C address
 
 // divide all these values by 17000 to get the amount in g force
-#define MIN_GYRO_BREAKING 35 // Buffer area where gyro wont do anything
-#define MAX_GYRO_BREAKING 550 // Max value of gyro when braking. anything more will be emergency braking
+#define MIN_GYRO_BREAKING           35             // Buffer area where gyro wont do anything
+#define MAX_GYRO_BREAKING           550            // Max value of gyro when braking. anything more will be emergency braking
 
-#define MIN_GYRO_ACCELERATING 800 // buffer area where gyro wont do anything
-#define MAX_GYRO_ACCELERATING 8000 // maximum value of gyro when accelerating
+#define MIN_GYRO_ACCELERATING       800            // buffer area where gyro wont do anything
+#define MAX_GYRO_ACCELERATING       8000           // maximum value of gyro when accelerating
 
-#define FORCE_EXPECTED_MAGNITUDE true  // Whether to override bad calibration values
-#define EXPECTED_ACC_MAGNITUDE 4115 // expected value of acceleration when stationary (gravity only). Defaults to this if calibration fails
-#define CALIBRATION_SAMPLE_SIZE 250 // sample size for calibration phase
-#define CALIBRATION_ACC_DELTA 2000 // ensures that the acc found during calibration is within this of EXPECTED_ACC_MAGNITUDE
+#define FORCE_EXPECTED_MAGNITUDE    true           // Whether to override bad calibration values
+#define EXPECTED_ACC_MAGNITUDE      4115           // expected value of acceleration when stationary (gravity only). Defaults to this if calibration fails
+#define CALIBRATION_SAMPLE_SIZE     250            // sample size for calibration phase
+#define CALIBRATION_ACC_DELTA       2000           // ensures that the acc found during calibration is within this of EXPECTED_ACC_MAGNITUDE
 
 // WARNING!!! MEDIAN_SAMPLE_SIZE IS VERY MEMORY INTENSIVE. STICK TO max 150 OR MEMORY WILL FRAGMENT
-#define MEDIAN_SAMPLE_SIZE 20 // higher value is more accurate, but slower. Used to detect hills
+#define MEDIAN_SAMPLE_SIZE          20             // higher value is more accurate, but slower. Used to detect hills
 
-// Finaly, it smoothes the previous and new values together instead of just setting them
-#define X_SMOOTHING 0.20
-#define Y_SMOOTHING 1
-#define Z_SMOOTHING 0.08 // smoothing for irregularities like bumps
-#define GLOBAL_SMOOTHING 0.225 // lower value is more smoothing, less vibrations, but less reactive/fast. 1 means no smoothing
+// Finally, it smoothes the previous and new values together instead of just setting them
+#define X_SMOOTHING                 0.20           // smoothing for X axis acceleration
+#define Y_SMOOTHING                 1              // smoothing for Y axis acceleration
+#define Z_SMOOTHING                 0.08           // smoothing for irregularities like bumps
+#define GLOBAL_SMOOTHING            0.225          // lower value is more smoothing, less vibrations, but less reactive/fast. 1 means no smoothing
 
 class Gyro {
     private:
         // Hardware references
-        Button* button;
+        Button*         button;
         
         // Calibration data
-        int idleAcc = 0;
+        int             idleAcc =                   0;
 
         // Sensor readings
-        int measuredAccX = 0;
-        int measuredAccY = 0;
-        int measuredAccZ = 0;
+        int             measuredAccX =              0;
+        int             measuredAccY =              0;
+        int             measuredAccZ =              0;
 
-        int smoothedAccX = 0;
-        int smoothedAccY = 0;
-        int smoothedAccZ = 0;
+        int             smoothedAccX =              0;
+        int             smoothedAccY =              0;
+        int             smoothedAccZ =              0;
 
-        int prevSmoothedAccX = 0;
-        int prevSmoothedAccY = 0;
-        int prevSmoothedAccZ = 0;
+        int             prevSmoothedAccX =          0;
+        int             prevSmoothedAccY =          0;
+        int             prevSmoothedAccZ =          0;
 
-        int sampleAccelerationMagnitudes[MEDIAN_SAMPLE_SIZE];
-        int numMedianSample = 0;
+        int             sampleAccelerationMagnitudes[MEDIAN_SAMPLE_SIZE];
+        int             numMedianSample =           0;
 
         // Filtered outputs
-        int correctedYAcceleration = 0;
+        int             correctedYAcceleration =    0;
 
         // Timing
-        unsigned long lastUpdateTime = 0;
+        unsigned long   lastUpdateTime =            0;
 
         bool readRawAccel();
         void initializeMPU();
         int median(int samples[], int size);
 
     public:
-        int smoothedAndCorrectedYAcc = 0;
-        int prevSmoothedAndCorrectedYAcc = 0;
+        int smoothedAndCorrectedYAcc =              0;
+        int prevSmoothedAndCorrectedYAcc =          0;
 
         Gyro(Button* button);
         void update();
